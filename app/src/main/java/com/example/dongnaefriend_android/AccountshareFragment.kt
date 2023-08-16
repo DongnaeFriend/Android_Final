9a6eb0f67d03935fd323d24045a072d737bd2a69
JuAh <cindykwak0809@gmail.com>
2023-08-16 17:10:25 +0900

    0816 복구

--- app/src/main/java/com/example/dongnaefriend_android/AccountshareFragment.kt
+++ app/src/main/java/com/example/dongnaefriend_android/AccountshareFragment.kt
@@ -1,19 +1,22 @@
 package com.example.dongnaefriend_android
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.dongnaefriend_android.adapter.AccountshareAdapter
 import com.example.dongnaefriend_android.databinding.FragmentAccountshareBinding
-import model.Post
+import model.AccountsharePost
 
 class AccountshareFragment : Fragment() {
     lateinit var binding: FragmentAccountshareBinding
     private lateinit var accountshareAdapter: AccountshareAdapter
-    private val accountshareData = mutableListOf<Post>()
+    private val accountshareData = mutableListOf<AccountsharePost>()
+    val TAG = "post"
 
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
@@ -31,8 +34,39 @@ class AccountshareFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        initAccountshareList()
+        arguments?.let {
+            val postType = it.getString("postType")
+            if (postType != null) {
+                initAccountshareList(postType)
+                initAccountshareRecyclerView()
+            }
+        }
+
+
+        initAccountshareList("all")
         initAccountshareRecyclerView()
+
+        binding.tvAccountshareAll.setOnClickListener {
+            initAccountshareList("all")
+            initAccountshareRecyclerView()
+        }
+        binding.tvAccountshareAgonize.setOnClickListener {
+            initAccountshareList("agonize")
+            initAccountshareRecyclerView()
+        }
+        binding.tvAccountshareBuy.setOnClickListener {
+            initAccountshareList("buy")
+            initAccountshareRecyclerView()
+        }
+        binding.tvAccountshareTip.setOnClickListener {
+            initAccountshareList("tip")
+            initAccountshareRecyclerView()
+        }
+        binding.tvAccountshareEtc.setOnClickListener {
+            initAccountshareList("etc")
+            initAccountshareRecyclerView()
+        }
+
 
         binding.btnAccountshare.setOnClickListener{
             parentFragmentManager.beginTransaction().replace(R.id.container_fragment,AccountshareWriteFragment()).commit()
@@ -47,53 +81,194 @@ class AccountshareFragment : Fragment() {
         binding.rvAccountshare.adapter = accountshareAdapter
     }
 
-    private fun initAccountshareList() {
+    private fun initAccountshareList(postType: String) {
+        Log.d("post", "postType:"+postType)
+
+        accountshareData.clear()
+
+
+        when (postType){
+            "all" -> {
+                binding.tvAccountshareAll.setTextColor(ContextCompat.getColor(requireActivity(), R.color.white))
+                binding.tvAccountshareAll.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.purple))
+                binding.tvAccountshareAgonize.setTextColor(ContextCompat.getColor(requireActivity(), R.color.black))
+                binding.tvAccountshareAgonize.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.white))
+                binding.tvAccountshareBuy.setTextColor(ContextCompat.getColor(requireActivity(), R.color.black))
+                binding.tvAccountshareBuy.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.white))
+                binding.tvAccountshareTip.setTextColor(ContextCompat.getColor(requireActivity(), R.color.black))
+                binding.tvAccountshareTip.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.white))
+                binding.tvAccountshareEtc.setTextColor(ContextCompat.getColor(requireActivity(), R.color.black))
+                binding.tvAccountshareEtc.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.white))
+                initAccountshareListAll()
+            }
+            "agonize" -> {
+                binding.tvAccountshareAll.setTextColor(ContextCompat.getColor(requireActivity(), R.color.black))
+                binding.tvAccountshareAll.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.white))
+                binding.tvAccountshareAgonize.setTextColor(ContextCompat.getColor(requireActivity(), R.color.white))
+                binding.tvAccountshareAgonize.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.purple))
+                binding.tvAccountshareBuy.setTextColor(ContextCompat.getColor(requireActivity(), R.color.black))
+                binding.tvAccountshareBuy.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.white))
+                binding.tvAccountshareTip.setTextColor(ContextCompat.getColor(requireActivity(), R.color.black))
+                binding.tvAccountshareTip.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.white))
+                binding.tvAccountshareEtc.setTextColor(ContextCompat.getColor(requireActivity(), R.color.black))
+                binding.tvAccountshareEtc.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.white))
+                initAccountshareListAgonize()
+            }
+            "buy" -> {
+                binding.tvAccountshareAll.setTextColor(ContextCompat.getColor(requireActivity(), R.color.black))
+                binding.tvAccountshareAll.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.white))
+                binding.tvAccountshareAgonize.setTextColor(ContextCompat.getColor(requireActivity(), R.color.black))
+                binding.tvAccountshareAgonize.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.white))
+                binding.tvAccountshareBuy.setTextColor(ContextCompat.getColor(requireActivity(), R.color.white))
+                binding.tvAccountshareBuy.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.purple))
+                binding.tvAccountshareTip.setTextColor(ContextCompat.getColor(requireActivity(), R.color.black))
+                binding.tvAccountshareTip.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.white))
+                binding.tvAccountshareEtc.setTextColor(ContextCompat.getColor(requireActivity(), R.color.black))
+                binding.tvAccountshareEtc.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.white))
+                initAccountshareListBuy()
+            }
+            "tip" -> {
+                binding.tvAccountshareAll.setTextColor(ContextCompat.getColor(requireActivity(), R.color.black))
+                binding.tvAccountshareAll.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.white))
+                binding.tvAccountshareAgonize.setTextColor(ContextCompat.getColor(requireActivity(), R.color.black))
+                binding.tvAccountshareAgonize.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.white))
+                binding.tvAccountshareBuy.setTextColor(ContextCompat.getColor(requireActivity(), R.color.black))
+                binding.tvAccountshareBuy.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.white))
+                binding.tvAccountshareTip.setTextColor(ContextCompat.getColor(requireActivity(), R.color.white))
+                binding.tvAccountshareTip.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.purple))
+                binding.tvAccountshareEtc.setTextColor(ContextCompat.getColor(requireActivity(), R.color.black))
+                binding.tvAccountshareEtc.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.white))
+                initAccountshareListTip()
+            }
+            "etc" -> {
+                binding.tvAccountshareAll.setTextColor(ContextCompat.getColor(requireActivity(), R.color.black))
+                binding.tvAccountshareAll.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.white))
+                binding.tvAccountshareAgonize.setTextColor(ContextCompat.getColor(requireActivity(), R.color.black))
+                binding.tvAccountshareAgonize.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.white))
+                binding.tvAccountshareBuy.setTextColor(ContextCompat.getColor(requireActivity(), R.color.black))
+                binding.tvAccountshareBuy.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.white))
+                binding.tvAccountshareTip.setTextColor(ContextCompat.getColor(requireActivity(), R.color.black))
+                binding.tvAccountshareTip.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.white))
+                binding.tvAccountshareEtc.setTextColor(ContextCompat.getColor(requireActivity(), R.color.white))
+                binding.tvAccountshareEtc.setBackgroundColor(ContextCompat.getColor(requireActivity(),R.color.purple))
+                initAccountshareListEtc()
+            }
+        }
+    }
+
+
+    private fun initAccountshareListAll() {
         accountshareData.addAll(
-            listOf<Post>(
-                Post(
-                    R.drawable.image_myaccount,
-                    "안녕하세요", "1", "3","본문입니다",
-                    "xx동","5","3"
-                 ),
-                Post(
-                    R.drawable.image_myaccount,
-                    "학교근처맛집", "1", "3","학교 근처에 먹을게 없어요 배고파요",
-                    "00동","5","3"
+            listOf<AccountsharePost>(
+                AccountsharePost(
+                    R.drawable.img_accountshare_dummy,
+                    "안녕하세요", "0", "0","본문입니다",
+                    "1","0",R.drawable.item_accountshare_category_untouched,"기타"
                 ),
-                Post(
-                    R.drawable.image_myaccount,
+                AccountsharePost(
+                    R.drawable.img_accountshare_dummy,
+                    "학교근처맛집", "0", "2","학교 근처에 먹을게 없어요 배고파요",
+                    "2","5",R.drawable.item_accountshare_category_untouched,"고민"
+                ),
+                AccountsharePost(
+                    R.drawable.img_accountshare_dummy,
                     "출근중인데", "1", "3","집에가고싶다...",
-                    "ㅁㅁ동","5","3"
+                    "3","5",R.drawable.item_accountshare_category_untouched,"고민"
                 ),
-                Post(
-                    R.drawable.image_myaccount,
-                    "오늘 저희 강아지가", "1", "3","휴지를 다 물어뜯어놨네요. 어떻게이런일이",
-                    "ㅎㅎ동","5","3"
+                AccountsharePost(
+                    R.drawable.img_accountshare_dummy,
+                    "서x웨이 싸게 먹는법", "3", "5","서x웨이 싸게 먹는 법 알려드립니다",
+                    "5","4",R.drawable.item_accountshare_category_untouched,"꿀팁"
                 ),
-                Post(
-                    R.drawable.image_myaccount,
+                AccountsharePost(
+                    R.drawable.img_accountshare_dummy,
                     "구내식당", "1", "3","노맛",
-                    "ㅇㅇ동","5","3"
+                    "7","13",R.drawable.item_accountshare_category_untouched,"기타"
                 ),
-                Post(
-                    R.drawable.image_myaccount,
-                    "안녕하세요", "1", "3","본문입니다",
-                    "ㄹㄹ동","5","3"
+                AccountsharePost(
+                    R.drawable.img_accountshare_dummy,
+                    "oo콘 vs xx깡", "15", "8","뭐 살까요",
+                    "9","50",R.drawable.item_accountshare_category_untouched,"살까말까"
                 ),
-                Post(
-                    R.drawable.image_myaccount,
-                    "안녕하세요", "1", "3","본문입니다",
-                    "ㅂㅂ동","5","3"
+                AccountsharePost(
+                    R.drawable.img_accountshare_dummy,
+                    "지금 oo마켓 할인행사", "1", "4","xx아파트 길건너 마트 행사하네요",
+                    "13","45",R.drawable.item_accountshare_category_untouched,"기타"
                 ),
-                Post(
-                    R.drawable.image_myaccount,
-                    "안녕하세요", "1", "3","본문입니다",
-                    "ㅎㅎ동","5","3"
+                AccountsharePost(
+                    R.drawable.img_accountshare_dummy,
+                    "돈을 너무 많이 썼어요", "5", "7","예산 초과가...",
+                    "15","29",R.drawable.item_accountshare_category_untouched,"고민"
                 )
 
             )
         )
     }
+    private fun initAccountshareListAgonize() {
+        accountshareData.addAll(
+            listOf<AccountsharePost>(
+                AccountsharePost(
+                    R.drawable.img_accountshare_dummy,
+                    "학교근처맛집", "0", "2","학교 근처에 먹을게 없어요 배고파요",
+                    "2","5",R.drawable.item_accountshare_category_untouched,"고민"
+                ),
+                AccountsharePost(
+                    R.drawable.img_accountshare_dummy,
+                    "출근중인데", "1", "3","집에가고싶다...",
+                    "3","5",R.drawable.item_accountshare_category_untouched,"고민"
+                ),
+                AccountsharePost(
+                    R.drawable.img_accountshare_dummy,
+                    "돈을 너무 많이 썼어요", "5", "7","예산 초과가...",
+                    "15","29",R.drawable.item_accountshare_category_untouched,"고민"
+                )
 
+            )
+        )
+    }
+    private fun initAccountshareListBuy() {
+        accountshareData.addAll(
+            listOf<AccountsharePost>(
+                AccountsharePost(
+                    R.drawable.img_accountshare_dummy,
+                    "oo콘 vs xx깡", "15", "8","뭐 살까요",
+                    "9","50",R.drawable.item_accountshare_category_untouched,"살까말까"
+                )
+            )
+        )
+    }
+    private fun initAccountshareListTip() {
+        accountshareData.addAll(
+            listOf<AccountsharePost>(
+                AccountsharePost(
+                    R.drawable.img_accountshare_dummy,
+                    "서x웨이 싸게 먹는법", "3", "5","서x웨이 싸게 먹는 법 알려드립니다",
+                    "5","4",R.drawable.item_accountshare_category_untouched,"꿀팁"
+                )
+
+            )
+        )
+    }
+    private fun initAccountshareListEtc() {
+        accountshareData.addAll(
+            listOf<AccountsharePost>(
+                AccountsharePost(
+                    R.drawable.img_accountshare_dummy,
+                    "안녕하세요", "0", "0","본문입니다",
+                    "1","0",R.drawable.item_accountshare_category_untouched,"기타"
+                ),
+                AccountsharePost(
+                    R.drawable.img_accountshare_dummy,
+                    "구내식당", "1", "3","노맛",
+                    "7","13",R.drawable.item_accountshare_category_untouched,"기타"
+                ),
+                AccountsharePost(
+                    R.drawable.img_accountshare_dummy,
+                    "지금 oo마켓 할인행사", "1", "4","xx아파트 길건너 마트 행사하네요",
+                    "13","45",R.drawable.item_accountshare_category_untouched,"기타"
+                )
+            )
+        )
+    }
 
 }
--- app/src/main/java/com/example/dongnaefriend_android/AccountshareWriteFragment.kt
+++ app/src/main/java/com/example/dongnaefriend_android/AccountshareWriteFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
 import com.example.dongnaefriend_android.databinding.FragmentAccountshareWriteBinding
 
 class AccountshareWriteFragment : Fragment() {
@@ -32,5 +33,73 @@ class AccountshareWriteFragment : Fragment() {
             intent.putExtra("text",2)
             startActivity(intent)
         }
+
+        binding.writeAgonize.setOnClickListener {
+            initAccountshareWriteList("agonize")
+        }
+
+        binding.writeBuy.setOnClickListener {
+            initAccountshareWriteList("buy")
+        }
+        binding.writeTip.setOnClickListener {
+            initAccountshareWriteList("tip")
+        }
+        binding.writeEtc.setOnClickListener {
+            initAccountshareWriteList("etc")
+        }
+
+        binding.accountshareWriteBack.setOnClickListener{
+            parentFragmentManager.beginTransaction().replace(R.id.container_fragment,AccountshareFragment()).commit()
+            (activity as AccountbookActivity).containerFragmentGone()
+        }
     }
+
+    private fun initAccountshareWriteList(s: String) {
+        when(s){
+            "agonize" ->{
+                binding.tvWriteAgonize.setTextColor(ContextCompat.getColor(requireActivity(), R.color.white))
+                binding.ivWriteAgonize.setImageResource(R.drawable.item_accountshare_category_touched)
+                binding.tvWriteBuy.setTextColor(ContextCompat.getColor(requireActivity(), R.color.grey))
+                binding.ivWriteBuy.setImageResource(R.drawable.item_accountshare_category_untouched_buy)
+                binding.tvWriteTip.setTextColor(ContextCompat.getColor(requireActivity(), R.color.grey))
+                binding.ivWriteTip.setImageResource(R.drawable.item_accountshare_category_untouched)
+                binding.tvWriteEtc.setTextColor(ContextCompat.getColor(requireActivity(), R.color.grey))
+                binding.ivWriteEtc.setImageResource(R.drawable.item_accountshare_category_untouched)
+            }
+
+            "buy" ->{
+                binding.tvWriteAgonize.setTextColor(ContextCompat.getColor(requireActivity(), R.color.grey))
+                binding.ivWriteAgonize.setImageResource(R.drawable.item_accountshare_category_untouched)
+                binding.tvWriteBuy.setTextColor(ContextCompat.getColor(requireActivity(), R.color.white))
+                binding.ivWriteBuy.setImageResource(R.drawable.item_accountshare_category_touched_buy)
+                binding.tvWriteTip.setTextColor(ContextCompat.getColor(requireActivity(), R.color.grey))
+                binding.ivWriteTip.setImageResource(R.drawable.item_accountshare_category_untouched)
+                binding.tvWriteEtc.setTextColor(ContextCompat.getColor(requireActivity(), R.color.grey))
+                binding.ivWriteEtc.setImageResource(R.drawable.item_accountshare_category_untouched)
+            }
+
+            "tip" ->{
+                binding.tvWriteAgonize.setTextColor(ContextCompat.getColor(requireActivity(), R.color.grey))
+                binding.ivWriteAgonize.setImageResource(R.drawable.item_accountshare_category_untouched)
+                binding.tvWriteBuy.setTextColor(ContextCompat.getColor(requireActivity(), R.color.grey))
+                binding.ivWriteBuy.setImageResource(R.drawable.item_accountshare_category_untouched_buy)
+                binding.tvWriteTip.setTextColor(ContextCompat.getColor(requireActivity(), R.color.white))
+                binding.ivWriteTip.setImageResource(R.drawable.item_accountshare_category_touched)
+                binding.tvWriteEtc.setTextColor(ContextCompat.getColor(requireActivity(), R.color.grey))
+                binding.ivWriteEtc.setImageResource(R.drawable.item_accountshare_category_untouched)
+            }
+
+            "etc" ->{
+                binding.tvWriteAgonize.setTextColor(ContextCompat.getColor(requireActivity(), R.color.grey))
+                binding.ivWriteAgonize.setImageResource(R.drawable.item_accountshare_category_untouched)
+                binding.tvWriteBuy.setTextColor(ContextCompat.getColor(requireActivity(), R.color.grey))
+                binding.ivWriteBuy.setImageResource(R.drawable.item_accountshare_category_untouched_buy)
+                binding.tvWriteTip.setTextColor(ContextCompat.getColor(requireActivity(), R.color.grey))
+                binding.ivWriteTip.setImageResource(R.drawable.item_accountshare_category_untouched)
+                binding.tvWriteEtc.setTextColor(ContextCompat.getColor(requireActivity(), R.color.white))
+                binding.ivWriteEtc.setImageResource(R.drawable.item_accountshare_category_touched)
+            }
+        }
+    }
+
 }
