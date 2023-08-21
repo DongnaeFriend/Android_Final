57e617eccce5ffe73d0479cc45c82d81862ad4b1
JuAh <cindykwak0809@gmail.com>
2023-08-21 22:52:23 +0800

    메인페이지 수정

--- app/src/main/java/com/example/dongnaefriend_android/HomeFragment.kt
+++ app/src/main/java/com/example/dongnaefriend_android/HomeFragment.kt
@@ -8,25 +8,38 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.dongnaefriend_android.databinding.FragmentHomeBinding
+import com.example.dongnaefriend_android.databinding.FragmentMypageProfileBinding
 
 
 class HomeFragment : Fragment() {
-    private lateinit var Binding: FragmentHomeBinding
+    lateinit var binding: FragmentHomeBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        Binding = FragmentHomeBinding.inflate(layoutInflater)
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-        Binding.tvAccountdetail.setOnClickListener {
-            val intent = Intent(context, AccountbookActivity::class.java)
-            startActivity(intent)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        /*
+        binding.goAccountbook.setOnClickListener{
+            parentFragmentManager.beginTransaction().replace(R.id.container_fragment,AccountshareFragment()).commit()
+        }
+        binding.goDongnae.setOnClickListener{
+            parentFragmentManager.beginTransaction().replace(R.id.container_fragment,DongnaeRestaurantFragment()).commit()
         }
 
-
-
-        return Binding.root
+         */
     }
 
 
