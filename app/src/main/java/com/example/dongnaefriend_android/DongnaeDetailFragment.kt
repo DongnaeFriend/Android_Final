bd98aaa2e1cbc0f95bad7982b9666b6a3d9f50bf
SeungJi20 <lina6709@naver.com>
2023-08-23 23:00:10 +0900

    프로필이미지 추가

--- app/src/main/java/com/example/dongnaefriend_android/DongnaeDetailFragment.kt
+++ app/src/main/java/com/example/dongnaefriend_android/DongnaeDetailFragment.kt
@@ -43,14 +43,16 @@ class DongnaeDetailFragment : Fragment() {
                     "구 바우하우스 근처에 24시 식당 괜찮은 곳 추천해주실 수 있나요?국밥은 너무 질려서 ㅠㅠ 기사식당 같은 곳도 괜찮아용!!"
             binding.infoTextView.text = "4분전 | 조회 22 | 댓글 0 | 공감 0"
             binding.locationTextView.text="장안동"
+            binding.iconImageView.setImageResource(R.drawable.profile_1)
             binding.detailImageView.setImageResource(R.drawable.restaurant_1)
 
         }
         if (sequence == "sequence=111"){
-            binding.reviewTextView.text = "드릅이나 오가피순무침 같은거 나물파는 반찬가게 있을까요?"
+            binding.reviewTextView.text = "드릅이나 오가피순무침 같은거 나물파는\n반찬가게 있을까요?"
             binding.reviewContentTextView.text = "반찬가게찾아요 아시는분계실까요"
             binding.infoTextView.text = "15분전 | 조회 30 | 댓글 1 | 공감 3"
             binding.locationTextView.text="전농동"
+            binding.iconImageView.setImageResource(R.drawable.profile_2)
             binding.detailImageView.setImageResource(R.drawable.restaurant_2)
 
         }
@@ -59,6 +61,7 @@ class DongnaeDetailFragment : Fragment() {
             binding.reviewContentTextView.text = "무한리필 식사하실 분? 명륜진사갈비 혼밥 가능할까요?"
             binding.infoTextView.text = "18분전 | 조회 22 | 댓글 2 | 공감 0"
             binding.locationTextView.text="장안동"
+            binding.iconImageView.setImageResource(R.drawable.profile_3)
             binding.detailImageView.setImageResource(R.drawable.restaurant_3)
 
         }
@@ -68,6 +71,7 @@ class DongnaeDetailFragment : Fragment() {
                     "너무 저렴하고 집밥같아서 소개해드려요\n"+"밑반찬도 맛있고 안짜고좋네요^^\n"+"제가 좋아하는 계란말이 매일 나온다네요\n"+"밥은 무한으로 먹을 수 있고\n"+"소식하는 분은 사진상 저처럼 한번에 많이드시지마시고 드시고 더드세요\n"+"가격이 5천원부터~ 가장 비싼가격이 6천원이라 부담없고 혼밥하시는 분들도 많이오시네요ㅋ"
             binding.infoTextView.text = "34분전 | 조회 50 | 댓글 6 | 공감 4"
             binding.locationTextView.text="휘경동"
+            binding.iconImageView.setImageResource(R.drawable.profile_4)
             binding.detailImageView.setImageResource(R.drawable.restaurant_4)
 
         }
@@ -77,6 +81,7 @@ class DongnaeDetailFragment : Fragment() {
                     "혼밥하기 쥑이네~\n\n"+"우연히 지나가다 들렸는데 점심특선 9800원\n"+"점심스페셜은 13000원 이건 양이\n"+"좀 많을듯해서 9800원짜리 점심 메뉴 픽!\n\n"+"괜.찮.다!^^\n"
             binding.infoTextView.text = "4시간전 | 조회 558 | 댓글 16 | 공감 0"
             binding.locationTextView.text="전농동"
+            binding.iconImageView.setImageResource(R.drawable.profile_5)
             binding.detailImageView.setImageResource(R.drawable.restaurant_5)
 
         }
@@ -85,6 +90,7 @@ class DongnaeDetailFragment : Fragment() {
             binding.reviewContentTextView.text = "별점 다섯개 박을게요\n"
             binding.infoTextView.text = "3일전 | 조회 464 | 댓글 5 | 공감 0"
             binding.locationTextView.text="장안동"
+            binding.iconImageView.setImageResource(R.drawable.profile_6)
             binding.detailImageView.setImageResource(R.drawable.restaurant_6)
 
         }
