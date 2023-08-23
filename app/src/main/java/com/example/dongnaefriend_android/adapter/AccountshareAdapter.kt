4f2d61d61188465a9b6e946e3bcef3fd18fc2a3b
SeungJi20 <lina6709@naver.com>
2023-08-23 14:54:33 +0900

    어댑터 수정

--- app/src/main/java/com/example/dongnaefriend_android/adapter/AccountshareAdapter.kt
+++ app/src/main/java/com/example/dongnaefriend_android/adapter/AccountshareAdapter.kt
@@ -1,16 +1,16 @@
 package com.example.dongnaefriend_android.adapter
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.example.dongnaefriend_android.AccountshareFragment
 import com.example.dongnaefriend_android.databinding.ItemAccountshareBinding
 import model.AccountsharePost
-import model.Post
 
 class AccountshareAdapter: RecyclerView.Adapter<AccountshareAdapter.ViewHolder>() {
 
     var dataList = mutableListOf<AccountsharePost>()
+    var onItemClick: ((AccountsharePost) -> Unit)? = null
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = ItemAccountshareBinding.inflate(LayoutInflater.from(parent.context),parent, false)
@@ -18,21 +18,33 @@ class AccountshareAdapter: RecyclerView.Adapter<AccountshareAdapter.ViewHolder>(
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.onBind(dataList[position])
+        val accountshare = dataList[position]
+        holder.onBind(accountshare)
     }
 
     override fun getItemCount(): Int = dataList.size
 
-    class ViewHolder(private val binding: ItemAccountshareBinding) : RecyclerView.ViewHolder(binding.root){
+    inner class ViewHolder(private val binding: ItemAccountshareBinding) : RecyclerView.ViewHolder(binding.root){
+
+        init {
+            itemView.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    val item = dataList[position]
+                    onItemClick?.invoke(item)
+                }
+            }
+        }
+
         fun onBind(accountshare: AccountsharePost){
             binding.ivItemAccountshareCover.setImageResource(accountshare.cover)
             binding.tvItemAccountshareTitle.text = accountshare.title
             binding.tvItemAccountshareMention.text = accountshare.mention
-            binding.tvItemAccountshareLike.text = accountshare.like
+            binding.tvItemAccountshareLike.text = accountshare.like.toString()
             binding.tvItemAccountshareText.text = accountshare.text
             binding.tvItemAccountshareTime.text = accountshare.time
-            binding.tvItemAccountshareNum.text = accountshare.num
-            binding.ivItemAccountshareCategory.setImageResource(accountshare.categooryimg)
+            binding.tvItemAccountshareNum.text = accountshare.num.toString()
+            binding.ivItemAccountshareCategory.setImageResource(accountshare.categooryimg)  // Ensure this property name is correct
             binding.tvItemAccountshareCategory.text = accountshare.categorytext
         }
     }
